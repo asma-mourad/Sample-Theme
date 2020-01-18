a04caf702269d4e4866329468d64bc6f854121d7
asma-mourad <amourad.2208@gmail.com>
2020-01-18 17:48:41 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".current-menu-item").addClass("colorlib-active");
+  $(".widget_categories").children("ul").addClass("categories");
+  $(".widget_archive").children("ul").addClass("categories");
+  $(".widget_recent_entries").children("ul").children("li").addClass("block-21 mb-4 d-flex");
+//  $(".comment-list").children("li").children("article").removeClass("comment-body");
+  
+
+});
